e3b271e25c64e40808b8d6a2124a3ca1223746c5
Igor Fonseca <igor.stutzf@gmail.com>
2023-11-11 19:26:53 -0300

    update home and header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,39 +1,42 @@
-import { useContext } from 'react'
-import { AuthContext } from '../../contexts/AuthContext'
-import logoImg from '../../assets/logo.svg'
-import { Link } from 'react-router-dom'
-import { FiLogIn } from 'react-icons/fi'
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
+import logoImg from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
 
 export function Header() {
   const { signed, loadingAuth } = useContext(AuthContext);
 
   return (
-    <div className="w-full flex items-center justify-center h-16 bg-white drop-shadow mb-4">
+    // O cabeçalho é fixado no topo da página e tem uma sombra aplicada para se destacar do conteúdo.
+    <div className="fixed top-0 left-0 right-0 z-50 flex items-center justify-center h-16 bg-white shadow-md">
       <header className="flex w-full max-w-7xl items-center justify-between px-4 mx-auto">
-         <Link to="/">
-            <img
-              src={logoImg} 
-              alt="Logo do site" 
-            />
-         </Link>
+        <Link to="/">
+          <img
+            src={logoImg} 
+            alt="Logo do site" 
+          />
+        </Link>
 
-         {!loadingAuth && signed && (
-          <Link to="/login">
-            <div className="border-2 rounded-full p-1 border-gray-900 flex justify-center">
+        {!loadingAuth && signed && (
+          <Link to="/login" className="flex items-center space-x-2">
+            <div className="border-2 rounded-full p-1 border-gray-900 flex justify-center items-center">
               <FiLogIn size={24} color="#000"/>
             </div>
             <span className="text-sm font-bold">Logout</span>
           </Link>
-         )}
+        )}
 
-        {/* {!loadingAuth && !signed && (
-          <Link to="/">
-            <div className="border-2 rounded-full p-1 border-gray-900 flex justify-center">
-              <FiLogIn size={16} color="#000"/>              
+        {/* Descomente e ajuste este bloco se você quiser mostrar um link de login/logout dependendo do estado de autenticação
+        {!loadingAuth && !signed && (
+          <Link to="/login" className="flex items-center space-x-2">
+            <div className="border-2 rounded-full p-1 border-gray-900 flex justify-center items-center">
+              <FiLogIn size={24} color="#000"/>              
             </div>
-            <span className="text-sm font-bold">login</span>
+            <span className="text-sm font-bold">Login</span>
           </Link>
-         )} */}
+        )}
+        */}
       </header>
     </div>
   )
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { collection, addDoc, doc, deleteDoc, query, where, onSnapshot, orderBy, updateDoc } from 'firebase/firestore';
+import { z } from 'zod';
 import { db } from '../../services/firebaseConnection';
 import { AuthContext } from '../../contexts/AuthContext';
 import { Container } from '../../components/container';
+
+const taskSchema = z.object({
+  title: z.string().nonempty("Task name cannot be empty."),
+});
 
 interface Task {
   id: string;
@@ -15,6 +20,7 @@ const App: React.FC = () => {
   const [newTask, setNewTask] = useState<string>("");
   const [editingTaskId, setEditingTaskId] = useState<string | null>(null);
   const [editingTaskTitle, setEditingTaskTitle] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   const { user } = useContext(AuthContext);
 
@@ -33,77 +39,101 @@ const App: React.FC = () => {
     }
   }, [user?.uid]);
   
+  const handleNewTaskChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTask(e.target.value);
+    setErrorMessage(null);
+  };
+
+  const handleEditingTaskTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEditingTaskTitle(e.target.value);
+    setErrorMessage(null);
+  };
 
   const addTask = async (): Promise<void> => {
-    if (newTask) {
-      try {
-        const timestamp = new Date();
-        await addDoc(collection(db, "tasks"), {
-          title: newTask,
-          userId: user?.uid,
-          createdAt: timestamp,
-        });
-        setNewTask("");
-      } catch (e) {
-        console.error("Error adding document: ", e);
-      }
+    setErrorMessage(null);
+    const result = taskSchema.safeParse({ title: newTask });
+    if (!result.success) {
+      setErrorMessage(result.error.errors[0].message);
+      return;
+    }
+
+    try {
+      const timestamp = new Date();
+      await addDoc(collection(db, "tasks"), {
+        title: newTask,
+        userId: user?.uid,
+        createdAt: timestamp,
+      });
+      setNewTask("");
+    } catch (e) {
+      console.error("Error adding document: ", e);
+      setErrorMessage("An error occurred while adding the task.");
     }
   };
-  
+
+  const editTask = async (taskId: string): Promise<void> => {
+    setErrorMessage(null);
+    const result = taskSchema.safeParse({ title: editingTaskTitle });
+    if (!result.success) {
+      setErrorMessage(result.error.errors[0].message);
+      return;
+    }
+
+    try {
+      const taskDocRef = doc(db, "tasks", taskId);
+      await updateDoc(taskDocRef, {
+        title: editingTaskTitle
+      });
+      setTasks(tasks.map(task => task.id === taskId ? { ...task, title: editingTaskTitle } : task));
+      setEditingTaskId(null);
+      setEditingTaskTitle("");
+    } catch (e) {
+      console.error("Erro ao atualizar o documento ", e);
+      setErrorMessage("An error occurred while updating the task.");
+    }
+  };
 
   const startEditing = (task: Task): void => {
     setEditingTaskId(task.id);
     setEditingTaskTitle(task.title);
-    setNewTask("");
   };
 
   const cancelEditing = (): void => {
     setEditingTaskId(null);
     setEditingTaskTitle("");
+    setErrorMessage(null); // Clear error message when cancelling editing
   };
-
-  const editTask = async (taskId: string): Promise<void> => {
-    if (editingTaskTitle) {
-      try {
-        const taskDocRef = doc(db, "tasks", taskId);
-        await updateDoc(taskDocRef, {
-          title: editingTaskTitle
-        });
-        setTasks(tasks.map(task => task.id === taskId ? { ...task, title: editingTaskTitle } : task));
-      } catch (e) {
-        console.error("Erro ao atualizar o documento ", e);
-      }
-    }
-    setEditingTaskId(null);
-    setEditingTaskTitle("");
-    setNewTask("");
-  };
-  
 
   const deleteTask = async (taskId: string): Promise<void> => {
-    await deleteDoc(doc(db, "tasks", taskId));
-    setTasks(tasks.filter(task => task.id !== taskId));
+    try {
+      await deleteDoc(doc(db, "tasks", taskId));
+      setTasks(tasks.filter(task => task.id !== taskId));
+    } catch (e) {
+      console.error("Erro ao deletar o documento ", e);
+      setErrorMessage("An error occurred while deleting the task.");
+    }
   };
 
   return (
     <Container>
-      <div className="p-6 bg-white shadow-md rounded-lg">
-        <h1 className="text-2xl font-bold text-gray-700 mb-4">My To-Do List</h1>
-        <div className="mt-4 flex flex-col sm:flex-row">
+      <div className="p-6 bg-white shadow-md rounded-lg mt-20">
+        <h1 className="text-2xl font-bold text-gray-700 mb-4">Minha lista de tarefas</h1>
+        <div className="flex flex-col sm:flex-row">
           <input
             type="text"
             value={newTask}
-            onChange={(e) => setNewTask(e.target.value)}
-            placeholder="Add a new task"
-            className="border p-2 rounded mb-2 sm:mb-0 sm:mr-2 flex-grow"
+            onChange={handleNewTaskChange}
+            placeholder="Digite o nome da sua tarefa"
+            className="border p-2 rounded flex-grow"
           />
-          <button onClick={addTask} className="bg-blue-500 hover:bg-blue-600 text-white p-2 rounded flex-grow sm:flex-grow-0">
-            Add Task
+          <button onClick={addTask} className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded mt-2 sm:mt-0 sm:ml-2">
+            Adicionar tarefa
           </button>
         </div>
+        {errorMessage && <p className="text-red-500 mt-2">{errorMessage}</p>}
         <ul className="mt-4">
           {tasks.map((task, index) => (
-            <li key={task.id} className="flex flex-col sm:flex-row items-center bg-gray-100 p-3 rounded-lg mb-2">
+            <li key={task.id} className="flex flex-col sm:flex-row bg-gray-100 p-3 rounded-lg mb-2">
               <span className="font-bold mr-2">{index + 1}.</span>
               <div className="flex-1 min-w-0 w-full">
                 {editingTaskId === task.id ? (
@@ -111,31 +141,30 @@ const App: React.FC = () => {
                     <input 
                       type="text" 
                       value={editingTaskTitle} 
-                      onChange={(e) => setEditingTaskTitle(e.target.value)}
-                      className="border p-2 rounded mb-2 sm:mb-0 w-full sm:flex-grow sm:mr-2"
+                      onChange={handleEditingTaskTitleChange}
+                      className="border p-2 rounded mb-2 sm:mb-0 w-full sm:mr-2"
                     />
                     <div className="flex space-x-2 mt-2 sm:mt-0">
-                      <button onClick={() => editTask(task.id)} className="bg-green-500 hover:bg-green-600 text-white p-1 rounded w-full sm:w-auto">
+                      <button onClick={() => editTask(task.id)} className="bg-green-500 hover:bg-green-700 text-white p-1 rounded flex-grow sm:flex-grow-0">
                         Save
                       </button>
-                      <button onClick={cancelEditing} className="bg-gray-500 hover:bg-gray-600 text-white p-1 rounded w-full sm:w-auto">
+                      <button onClick={cancelEditing} className="bg-gray-500 hover:bg-gray-700 text-white p-1 rounded flex-grow sm:flex-grow-0">
                         Cancel
                       </button>
                     </div>
                   </div>
                 ) : (
-                  <div className="flex-1 min-w-0">
-                    <span className="text-gray-700 block break-all overflow-wrap break-word">{task.title}</span>
-                  </div>
+                  <span className="text-gray-700 break-all overflow-wrap break-word">{task.title}</span>
                 )}
+                {errorMessage && editingTaskId === task.id && <p className="text-red-500 mt-2">{errorMessage}</p>}
               </div>
               {editingTaskId !== task.id && (
                 <div className="flex space-x-2 mt-2 sm:mt-0">
-                  <button onClick={() => startEditing(task)} className="bg-yellow-500 hover:bg-yellow-600 text-white p-1 rounded w-full sm:w-auto">
-                    Edit
+                  <button onClick={() => startEditing(task)} className="bg-yellow-500 hover:bg-yellow-700 text-white p-1 rounded flex-grow">
+                    Editar
                   </button>
-                  <button onClick={() => deleteTask(task.id)} className="bg-red-500 hover:bg-red-600 text-white p-1 rounded w-full sm:w-auto">
-                    Delete
+                  <button onClick={() => deleteTask(task.id)} className="bg-red-500 hover:bg-red-700 text-white p-1 rounded flex-grow">
+                    Excluir
                   </button>
                 </div>
               )}

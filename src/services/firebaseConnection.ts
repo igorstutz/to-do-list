929e541e4565891fd22969c44031f05dc7ee01e3
Igor Fonseca <igor.stutzf@gmail.com>
2023-12-26 15:26:23 -0300

    Revert "Revert "Revert "update sistem"""
    This reverts commit 9d52a4ce65b4c762c2ad426a24ca3a7afd526ade.

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -7,12 +7,12 @@ import { getStorage } from 'firebase/storage'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD9uYc3z15bGDSLM3yPYcztNhuxPfoLLUI",
-  authDomain: "ldo-list-e814b.firebaseapp.com",
-  projectId: "ldo-list-e814b",
-  storageBucket: "ldo-list-e814b.appspot.com",
-  messagingSenderId: "965168510773",
-  appId: "1:965168510773:web:b9fd617be3d3433de83e7c"
+  apiKey: "AIzaSyDfE6b1Dxq_lrDMK86nvql5hyFwRuKah0E",
+  authDomain: "to-do-list-bfc82.firebaseapp.com",
+  projectId: "to-do-list-bfc82",
+  storageBucket: "to-do-list-bfc82.appspot.com",
+  messagingSenderId: "434951407359",
+  appId: "1:434951407359:web:cfa65ad22d9a42ca71aeca"
 };
 
 const app = initializeApp(firebaseConfig);
